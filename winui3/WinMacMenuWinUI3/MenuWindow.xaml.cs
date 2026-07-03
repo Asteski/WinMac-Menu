eb1095a396d39b578402acf8c3640225fc7aa638
asteski <asteski@outlook.com>
2026-07-03 11:18:33 +0200

    fix

--- winui3/WinMacMenuWinUI3/MenuWindow.xaml.cs
+++ winui3/WinMacMenuWinUI3/MenuWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 using Windows.Foundation;
 using Windows.Graphics;
 using WinMacMenuWinUI3.Models;
